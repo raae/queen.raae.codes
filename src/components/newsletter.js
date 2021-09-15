6b92705fe4eca7bef3108cf25e23c68801914be7
Benedicte Raae <raae@raae.codes>
2021-09-15 11:49:45 +0200

    Newsletter form

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,15 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { addSubscriber } from "../services/subscriptions";
 
-const NewsletterForm = ({ children, cta = "Get notifed" }) => {
+const TEXT = {
+  PENDING: <>Hold on...</>,
+  FULFILLED: (
+    <>
+      Almost there! <br />
+      Check your inbox to confirm...
+    </>
+  ),
+  FAILED: <>Oh no...something went wrong...</>,
+};
+
+const NewsletterForm = ({
+  subscription = "queen",
+  children,
+  cta = "Get notifed",
+  tags = [],
+}) => {
+  const [status, setStatus] = useState("INITIAL");
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+    setStatus("PENDING");
+
+    try {
+      await addSubscriber({
+        email: event.target.elements.email.value,
+        subscription: subscription,
+        tags: tags,
+      });
+      setStatus("FULFILLED");
+    } catch (error) {
+      console.warn(error);
+      setStatus("FAILED");
+    }
+  };
+
+  const handleDismiss = async (event) => {
+    event.preventDefault();
+    setStatus("INITIAL");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
       {children}
 
-      <fieldset>
+      <fieldset disabled={status === "pending"}>
         <label htmlFor="email">Your email address:</label>
-        <input id="email" type="email" />
+        <input id="email" name="email" type="email" required />
         <button type="submit">{cta}</button>
       </fieldset>
+
+      {status !== "INITIAL" && (
+        <aside>
+          {status === "FAILED" && (
+            <button type="button" onClick={handleDismiss}>
+              X
+            </button>
+          )}
+          <strong>{TEXT[status]}</strong>
+        </aside>
+      )}
     </form>
   );
 };
