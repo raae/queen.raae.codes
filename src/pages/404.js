0474aa1f5f623a3785ac29cd7e3ac6a43b417120
Benedicte Raae <raae@raae.codes>
2021-09-16 18:16:36 +0200

    Tiny 404 fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,29 +5,37 @@ import Seo from "../components/seo";
 
 import MainMenu from "../content/main-menu";
 import SocialLinks from "../content/social-links";
+import NewsletterSection from "../content/newsletter-section";
 
 const NotFoundPage = (props) => {
   return (
     <Layout>
       <Seo {...props} meta={{ title: "Not found" }} />
+      <main>
+        <header>
+          <h1>
+            Sorry{" "}
+            <span role="img" aria-label="Pensive emoji">
+              😔
+            </span>{" "}
+            we couldn’t find what you were looking for.
+          </h1>
+          <nav>
+            <MainMenu />
+            <SocialLinks />
+          </nav>
+        </header>
 
-      <header>
-        <h1>
-          Sorry{" "}
-          <span role="img" aria-label="Pensive emoji">
-            😔
-          </span>{" "}
-          we couldn’t find what you were looking for.
-        </h1>
-        <nav>
-          <MainMenu />
-          <SocialLinks />
-        </nav>
-      </header>
-
-      <section>
-        <img src="https://http.cat/404" alt="Cat hiding, but clearly visible" />
-      </section>
+        <section>
+          <img
+            src="https://http.cat/404"
+            alt="Cat hiding, but clearly visible"
+          />
+        </section>
+      </main>
+      <footer>
+        <NewsletterSection />
+      </footer>
     </Layout>
   );
 };
