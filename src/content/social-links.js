54e0789141e441063dbc1eaa2824af6c46d5ebee
Benedicte Raae <raae@raae.codes>
2022-01-12 21:07:20 +0100

    fix: youtube link, closes #39

--- src/content/social-links.js
+++ src/content/social-links.js
@@ -16,7 +16,7 @@ export default SocialLinks;
 
 const MENU = [
   { label: "Email (queen@raae.codes)", href: "mailto:queen@raae.codes" },
-  { label: "YouTube", href: "https://youtube.com/raaecodes" },
+  { label: "YouTube", href: "https://youtube.com/QueenRaae" },
   { label: "Twitter", href: "https://twitter.com/raae" },
   { label: "GitHub", href: "https://github.com/queen-raae" },
 ];
