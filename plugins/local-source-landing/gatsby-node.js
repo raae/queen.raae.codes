214ea2f90453fe3badd431a777c03282d91a1dae
Benedicte Raae <raae@raae.codes>
2022-01-30 14:13:35 +0100

    refactor:Testimonials

--- plugins/local-source-landing/gatsby-node.js
+++ plugins/local-source-landing/gatsby-node.js
@@ -30,4 +30,28 @@ exports.onCreateNode = async ({
       },
     });
   }
+
+  if (
+    node.internal.type === "File" &&
+    node.sourceInstanceName.includes("Landing") &&
+    node.internal.mediaType === "text/markdown" &&
+    // Do not make landing pages of sections
+    node.relativePath.includes("_testimonials")
+  ) {
+    const slug = createFilePath({ node, getNode });
+    const content = await loadNodeContent(node);
+    const type = "Testimonial";
+
+    createNode({
+      ...node,
+      id: createNodeId(`${node.id} >>> ${type}`),
+      slug: slug,
+      internal: {
+        content: content,
+        mediaType: node.internal.mediaType,
+        contentDigest: node.internal.contentDigest,
+        type: type,
+      },
+    });
+  }
 };
