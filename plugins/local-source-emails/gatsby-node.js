ff3d6fab33b693bc4e8a31f54f4760adec5470d6
Benedicte Raae <raae@raae.codes>
2022-02-16 10:43:55 +0100

    refactor: year and month folders for emails (#75)

--- plugins/local-source-emails/gatsby-node.js
+++ plugins/local-source-emails/gatsby-node.js
@@ -1,8 +1,8 @@
 const { isString } = require("lodash");
 const { createFilePath } = require("gatsby-source-filesystem");
 
-const NOW = new Date().toISOString();
-const FAR_FUTURE = new Date("2300-01-01").toISOString();
+const NOW = new Date().toISOString().substring(0, 10);
+const FAR_FUTURE = "2300-01-01";
 const CUT_OFF = process.env.NODE_ENV === "development" ? FAR_FUTURE : NOW;
 
 exports.onCreateNode = async (
@@ -25,29 +25,35 @@ exports.onCreateNode = async (
       reporter.panic("Email pages need a base path");
     }
 
-    const slug = options.basePath + createFilePath({ node, getNode });
+    const filePath = createFilePath({ node, getNode });
     const content = await loadNodeContent(node);
     const type = node.sourceInstanceName;
 
-    const pattern = /\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])/;
+    const pattern = /((\d{4})\/(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01]))-(.*)/;
     const dateSearch = pattern.exec(node.relativeDirectory);
-    const date = dateSearch
-      ? new Date(dateSearch[0]).toISOString()
-      : new Date(0).toISOString();
 
-    if (date <= CUT_OFF) {
-      createNode({
-        id: createNodeId(`${node.id} >>> ${type}`),
-        dir: node.dir,
-        slug: slug,
-        date: date,
-        internal: {
-          content: content,
-          mediaType: node.internal.mediaType,
-          contentDigest: node.internal.contentDigest,
-          type: type,
-        },
-      });
+    try {
+      const dateString = `${dateSearch[2]}-${dateSearch[3]}-${dateSearch[4]}`;
+      const slug = `${options.basePath}/${dateString}-${dateSearch[5]}/`;
+
+      if (dateString <= CUT_OFF) {
+        createNode({
+          id: createNodeId(`${node.id} >>> ${type}`),
+          dir: node.dir,
+          slug: slug,
+          date: dateString,
+          internal: {
+            content: content,
+            mediaType: node.internal.mediaType,
+            contentDigest: node.internal.contentDigest,
+            type: type,
+          },
+        });
+
+        reporter.info(`${type} created for ${filePath} at ${slug} `);
+      }
+    } catch (error) {
+      reporter.warn(`${type} for ${filePath} failed: ${error.message}`);
     }
   }
 
