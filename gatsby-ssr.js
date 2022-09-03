6c4ecf42e2ede782d9ec875f43f08fab678ea9cd
Benedicte Raae <raae@raae.codes>
2022-09-03 09:40:21 +0200

    fix: html lang not set by Gatsby Head API

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 export const onRenderBody = (gatsbyUtils) => {
-  const { setHeadComponents } = gatsbyUtils;
+  const { setHeadComponents, setHtmlAttributes } = gatsbyUtils;
+
+  setHtmlAttributes({ lang: "en" });
 
   setHeadComponents([
     <style key="base-font-size">
