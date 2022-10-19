3fab4ed2662da0a1230324659faac999bf5ff0c1
Benedicte Raae <raae@raae.codes>
2022-10-19 16:13:48 +0200

    refactor: tailwind testimonial

--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -1,6 +1,6 @@
 import React from "react";
+import clsx from "clsx";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import { Avatar, Box, Link } from "@mui/material";
 import Prose from "./prose";
 
 const Testimonial = ({
@@ -11,29 +11,36 @@ const Testimonial = ({
   attended,
   children,
   sx,
+  className,
   ...props
 }) => {
   return (
-    <Box
+    <blockquote
       component="blockquote"
+      className={clsx(
+        "m-0 px-6 pt-3 pb-5 border-solid border-4 border-yellow-400",
+        className
+      )}
       {...props}
-      sx={{
-        border: 4,
-        px: "1.5em",
-        pt: "0.5em",
-        borderColor: "secondary.main",
-        m: 0,
-        ...sx,
-      }}
     >
       <Prose>{children}</Prose>
-      <Box sx={{ display: "flex", alignItems: "center", my: "1.5em" }}>
-        <Avatar sx={{ width: "5rem", height: "5rem", mr: 3 }}>
-          <GatsbyImage image={getImage(avatar)} alt={who} />
-        </Avatar>
-        <Box component="cite">
-          {who && !url && <>{who}</>}
-          {who && url && <Link href={url}>{who}</Link>}
+      <div className="flex items-center gap-4 mt-6">
+        <GatsbyImage
+          className="h-20 w-20 rounded-full drop-shadow"
+          image={getImage(avatar)}
+          alt={who}
+        />
+
+        <cite className="text-sm leading-tight">
+          {who && !url && <span className="font-semibold">{who}</span>}
+          {who && url && (
+            <a
+              className="font-semibold text-inherit underline-offset-2 decoration-transparent hover:decoration-amber-600 transition"
+              href={url}
+            >
+              {who}
+            </a>
+          )}
 
           {attended && (
             <>
@@ -43,12 +50,17 @@ const Testimonial = ({
           {company && (
             <>
               <br />
-              <Link href={company.url}>{company.name}</Link>
+              <a
+                className="font-semibold text-inherit underline-offset-2 decoration-transparent hover:decoration-amber-600 transition"
+                href={company.url}
+              >
+                {company.name}
+              </a>
             </>
           )}
-        </Box>
-      </Box>
-    </Box>
+        </cite>
+      </div>
+    </blockquote>
   );
 };
 
